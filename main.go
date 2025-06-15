30318d47387fad702895b10e0cedd6bd8d3ff3d7
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-06-16 01:51:58 +0800

    feat mysql connection

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"gift/repo/models"
+	"gift/util/mysqlutil"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	//fmt.Println("Hello World")
+	db, err := mysqlutil.NewMysqlClient()
+	if err != nil {
+		log.Fatalln("数据库连接失败", err)
+	}
+	//fmt.Printf("mysqlDb = %#v\n", db)
+	// 自动创建表
+	if err = db.AutoMigrate(models.Inventory{}); err != nil {
+		log.Fatalf("自动迁移失败: %v", err)
+	}
+
+	// repo层初始化
+
+	// service层初始化
+
+	// api层初始化
+
 }
