1fef8ef936450f26598db981e5a29a921fa78e7e
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-06-17 23:00:32 +0800

    feat basic server frame

--- main.go
+++ main.go
@@ -3,40 +3,88 @@ package main
 import (
 	"context"
 	"fmt"
+	"gift/api"
+	"gift/repo"
 	"gift/repo/models"
+	"gift/services"
 	"gift/util/mysqlutil"
 	"gift/util/redisutil"
+	"github.com/gin-gonic/gin"
 	"log"
 )
+
+// SetRoute 路由组设置
+func SetRoute(router gin.IRouter, inventoryApi api.Inventory) {
+	v1 := router.Group("/v1")
+	{
+		inventoryNotLogin := v1.Group("/inventory")
+		{
+			inventoryNotLogin.POST("/create", inventoryApi.Create) // 增加新物品
+			inventoryNotLogin.POST("/update", inventoryApi.Update) // 更新物品信息
+			inventoryNotLogin.POST("/delete", inventoryApi.Delete) // 删除物品
+		}
+	}
+}
+
+// NewEngine 初始化新engine
+func NewEngine(inventoryApi api.Inventory) *gin.Engine {
+	gin.SetMode(gin.DebugMode)
+	// 启用自定义的引擎
+	engine := gin.New()
+
+	// TODO 注册全局中间件
+	// gin.Recovery是内置的回复中间件，用于捕获程序中的panic，防止服务崩溃，并返回500错误
+	//engine.Use(gin.Recovery(), middleware.RequestID(), middleware.GinLogger())
+	engine.Use(gin.Logger(), gin.Recovery())
+
+	// 设置路由
+	SetRoute(
+		engine,
+		inventoryApi,
+	)
+	return engine
+}
 
 func main() {
 	// 根context
 	rootCtx := context.Background()
 
-	// 连接Mysql
+	///////////////////////////////////////////// 连接Mysql /////////////////////////////////////////////////
 	db, err := mysqlutil.NewMysqlClient(rootCtx)
 	if err != nil {
 		log.Fatalln("mysql连接失败", err)
 	}
-	fmt.Printf("mysqlDb = %#v\n", db)
-
+	fmt.Println("Mysql初始化成功")
 	// 自动创建表
 	if err = db.AutoMigrate(models.Inventory{}, models.Order{}); err != nil {
 		log.Fatalf("自动迁移失败: %v", err)
 	}
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// -> db 全局mysql数据库
 
-	// 连接Redis
+	///////////////////////////////////////////// 连接Redis /////////////////////////////////////////////////
 	rds, err := redisutil.NewRedisClient(rootCtx)
 	if err != nil {
 		log.Fatalln("redis连接失败", err)
 	}
 
-	fmt.Println(rds)
+	fmt.Println("Redis初始化成功")
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// -> rds全局redis数据库
 
+	/////////////////////////////////////////// 业务逻辑初始化 ///////////////////////////////////////////////
 	// repo层初始化
+	inventoryRepo := repo.NewInventory(db)
 
 	// service层初始化
+	inventorySvc := services.NewInventory(inventoryRepo)
 
 	// api层初始化
+	inventoryApi := api.NewInventory(inventorySvc)
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+	// 实例化gin引擎
+	engine := NewEngine(inventoryApi)
+	// 在本地8080端口开启服务
+	_ = engine.Run(":8080")
 }
