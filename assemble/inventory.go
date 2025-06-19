6cd488be37eafe4884963611cf79b96d47132b1e
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-06-19 17:22:25 +0800

    fix inventory Model2Info

--- assemble/inventory.go
+++ assemble/inventory.go
@@ -6,12 +6,13 @@ import (
 )
 
 func Model2Info(list []*models.Inventory) []*dto.InventoryInfo {
-	infoList := make([]*dto.InventoryInfo, len(list))
+	infoList := make([]*dto.InventoryInfo, 0)
 	for _, v := range list {
 		infoList = append(infoList, &dto.InventoryInfo{
 			ID:          v.ID,
 			Name:        v.Name,
 			Description: v.Description,
+			Picture:     v.Picture,
 			Price:       v.Price,
 			Count:       v.Count,
 			CreatedAt:   uint64(v.CreatedAt.Unix()),
