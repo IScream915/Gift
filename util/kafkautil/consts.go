61ecd3c1389a33e65e98304c8bec249470c242bc
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-07-14 01:58:21 +0800

    fix kafka配置文件导入路径

--- util/kafkautil/consts.go
+++ util/kafkautil/consts.go
@@ -2,5 +2,5 @@ package kafkautil
 
 const (
 	// DefaultConfigPath 这里这么写是因为test文件的工作路径在本文件夹，因此需要定位到根工作目录
-	DefaultConfigPath = "../../config/kafkaconfig.yaml"
+	DefaultConfigPath = "./config/kafkaconfig.yaml"
 )
